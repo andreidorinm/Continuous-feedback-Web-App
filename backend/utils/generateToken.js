d1767180887e3d76d07ab94c5553842e2ed9ff1b
andreidorinm <andrei.dorin09@yahoo.com>
2022-12-22 01:20:11 +0200

    added gitignore

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,8 +1,8 @@
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
 
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "30d",
+    expiresIn: '30d',
   });
 };
 
