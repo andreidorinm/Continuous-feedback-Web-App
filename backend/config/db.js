baccf389ef2b8bd59bdd5138cd3e3e2414ddb507
andreidorinm <andrei.dorin09@yahoo.com>
2022-12-22 00:40:09 +0200

    created backend

---
+++ backend/config/db.js
@@ -0,0 +1,9 @@
+import sequelize from "sequelize";
+
+const connectDB = async () => {
+  //Mihail aici tu bagi conexiunea la baza de date
+  try {
+  } catch (error) {}
+};
+
+export default sequelize;
