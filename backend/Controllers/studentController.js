34367448b83549b8784c895cf41564e4126c8b57
Let Andrei <letandrei20@stud.ase.ro>
2023-01-06 19:11:30 +0200

    update controller for student

--- backend/Controllers/studentController.js
+++ backend/Controllers/studentController.js
@@ -1,4 +1,25 @@
 import { Student } from "../Models/student.js";
+import { Participation } from "../Models/participation.js";
+
+//get all students by activity id
+const getStudentsByActivity = async (req, res, next) => {
+    try {
+        const activityId = req.params.activityId;
+        const students = await Student.findAll({
+            include: [{
+                model: Participation,
+                as: 'participations',
+                where: { activityId }
+            }]
+        });
+        if (students.length === 0)
+            res.status(404).json({ message: 'No students found.' })
+        else
+            res.status(200).json({ data: students });
+    } catch (error) {
+        next(error);
+    }
+};
 
 //get student by id
 const getStudentById = async (req, res, next) => {
@@ -15,5 +36,6 @@ const getStudentById = async (req, res, next) => {
 };
 
 export {
-    getStudentById
+    getStudentById,
+    getStudentsByActivity
 }
