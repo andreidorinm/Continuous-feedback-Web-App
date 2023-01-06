49ba68db5bf9a193464a34b0377752b3dadcc237
Let Andrei <letandrei20@stud.ase.ro>
2023-01-06 18:59:27 +0200

    updated routers for prof and stud

--- backend/Routes/professorRouter.js
+++ backend/Routes/professorRouter.js
@@ -1,59 +1,13 @@
 import express from "express";
-import {Professor} from "../Models/professor.js";
+import * as professorController from "../Controllers/professorController.js"
+import * as activityController from "../Controllers/activityController.js"
 
 const professorRouter = express.Router();
 
-//get all proferssors
-professorRouter.get("/allProfessors", async (request, response, next) => {
-    try {
-      const proferssors = await Professor.findAll();
-      if (proferssors.length > 0) {
-        response.json(proferssors);
-      } else {
-        response.sendStatus(204);
-      }
-    } catch (error) {
-      next(error);
-    }
-  });
-
 //get professor by id
-professorRouter.get(
-    "/:professorId",
-    async (request, response, next) => {
-      try {
-        const professor = await Professor.findByPk(request.params.professorId);
-          if (professor) {
-            response.json(professor);
-          } else {
-            response.sendStatus(404);
-          }
-      } catch (error) {
-        next(error);
-      }
-    }
-  );
+professorRouter.get("/:professorId", professorController.getProfessorById);
 
-  //get activities by professor id
-  professorRouter.get(
-    "/:professorId/activities",
-    async (request, response, next) => {
-      try {
-        const professor = await Professor.findByPk(request.params.professorId);
-        if (professor) {
-          const activities = await professor.getActivities();
-          if (activities.length > 0) {
-            response.json(activities);
-          } else {
-            response.sendStatus(204);
-          }
-        } else {
-          response.sendStatus(404);
-        }
-      } catch (error) {
-        next(error);
-      }
-    }
-  );
+//get activities by professor id
+professorRouter.get("/:professorId/activities", activityController.getActivitiesByProfessor)
 
-  
+export { professorRouter }
