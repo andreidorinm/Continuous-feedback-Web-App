f8e2c4e87583b9a7222a275dc08044048eedd857
MilotoiuMihail <milotoiu.mihail1@gmail.com>
2023-01-06 18:31:08 +0200

    deleted unnecessary files

--- backend/config/db.js
+++
@@ -1,9 +0,0 @@
-import sequelize from "sequelize";
-
-const connectDB = async () => {
-  //Mihail aici tu bagi conexiunea la baza de date
-  try {
-  } catch (error) {}
-};
-
-export default sequelize;
