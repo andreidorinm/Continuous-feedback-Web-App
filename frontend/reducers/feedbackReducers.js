9a49bb004261510c78cdb44d286f04baaca153ea
Andrei Mihaila <73621790+andreidorinm@users.noreply.github.com>
2023-01-07 10:23:09 +0200

    frontend started (#7)
    * linting and updated naming
    
    * upgrade packages
    
    * added constants for redux
    
    * added some components
    
    * added the app arhitecture
    
    * refactoring and upgraded packages be
    
    Co-authored-by: MilotoiuMihail <69942328+MilotoiuMihail@users.noreply.github.com>

--- frontend/reducers/feedbackReducers.js
+++ frontend/reducers/feedbackReducers.js
@@ -2,6 +2,10 @@ import {
   FEEDBACK_LIST_REQUEST,
   FEEDBACK_LIST_SUCCESS,
   FEEDBACK_LIST_FAIL,
+  FEEDBACK_CREATE_REQUEST,
+  FEEDBACK_CREATE_SUCCESS,
+  FEEDBACK_CREATE_FAIL,
+  FEEDBACK_CREATE_RESET,
 } from '../constants/feedbackConstants.js';
 
 export const feedbackListReducer = (state = { feedbacks: [] }, action) => {
@@ -12,6 +16,21 @@ export const feedbackListReducer = (state = { feedbacks: [] }, action) => {
       return { loading: false, feedbacks: action.payload };
     case FEEDBACK_LIST_FAIL:
       return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const feedbackCreateReducer = (state = {}, action) => {
+  switch (action.type) {
+    case FEEDBACK_CREATE_REQUEST:
+      return { loading: true };
+    case FEEDBACK_CREATE_SUCCESS:
+      return { loading: false, success: true, feedback: action.payload };
+    case FEEDBACK_CREATE_FAIL:
+      return { loading: false, error: action.payload };
+    case FEEDBACK_CREATE_RESET:
+      return {};
     default:
       return state;
   }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Header from '../components/Header';
+import HomeScreen from '../screens/HomeScreen';
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <span>In Development</span>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path="/" component={HomeScreen} />
+          <Route path="/activity" component={ActvityScreen} />
+          <Route path="/feedback" component={FeedbackScreen} />
+          <Route path="/student" component={StudentScreen} />
+          <Route path="/professor" component={ProfessorScreen} />
+          <Route path="/login" component={LoginScreen} />
+          <Route path="/register" component={RegisterScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 };
 
