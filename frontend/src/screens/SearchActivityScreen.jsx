a589b7dbc69ad9ce6597db5001ddf92423195acb
andreidorinm <andrei.dorin09@yahoo.com>
2023-01-07 23:39:20 +0200

    added what gets called

--- frontend/src/screens/SearchActivityScreen.jsx
+++ frontend/src/screens/SearchActivityScreen.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react"
-import axios from "axios"
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 const ActivityScreen = () => {
@@ -7,31 +7,60 @@ const ActivityScreen = () => {
   const [results, setResults] = useState([]);
   const navigate = useNavigate();
 
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value);
-  }
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5001/api/activities/search?q=${searchTerm}`)
+      .then((response) => {
+        setResults(response.data.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [searchTerm]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    axios.get(`http://localhost:5001/api/activities/search?q=${searchTerm}`)
-      .then(response => {
+    axios
+      .get(`http://localhost:5001/api/activities/search?q=${searchTerm}`)
+      .then((response) => {
         setResults(response.data.data);
         if (response.data.data.length > 0) {
-          navigate(`/activities/${response.data.data[0].id}`, { state: { activity: response.data.data[0] } });
+          navigate(`/activities/${response.data.data[0].id}`, {
+            state: { activity: response.data.data[0] },
+          });
           location.reload();
         }
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
-  }
+  };
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleClick = (activity) => {
+    navigate(`/activities/${activity.id}`, { state: { activity } });
+  };
 
   return (
     <form onSubmit={handleSubmit}>
       <input type="text" value={searchTerm} onChange={handleChange} />
-      <button type="submit">Search</button>
+        <button type="submit" >
+          Search
+        </button>
+      {results.length > 0 && searchTerm !== '' && (
+        <ul>
+          {results.map((result) => (
+            <li key={result.id} onClick={() => handleClick(result)}>
+              {result.title}
+            </li>
+          ))}
+        </ul>
+      )}
     </form>
   );
-}
+};
 
-export default ActivityScreen
+export default ActivityScreen;
