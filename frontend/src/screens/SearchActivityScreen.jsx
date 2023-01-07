fd2c23a853dda993f4ff3e38a7c984f8223e4f25
andreidorinm <andrei.dorin09@yahoo.com>
2023-01-07 22:47:56 +0200

    refactored the redirect for be

--- frontend/src/screens/SearchActivityScreen.jsx
+++ frontend/src/screens/SearchActivityScreen.jsx
@@ -5,18 +5,7 @@ import { useNavigate } from 'react-router-dom';
 const ActivityScreen = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [results, setResults] = useState([]);
-  const [data, setData] = useState([]);
   const navigate = useNavigate();
-
-  useEffect(() => {
-    axios.get('http://localhost:5001/api/activities')
-      .then(response => {
-        setData(response.data);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }, []);
 
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
@@ -24,22 +13,29 @@ const ActivityScreen = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const filteredData = data.data.filter(activity => activity.title.toLowerCase().includes(searchTerm.toLowerCase()));
-    setResults(filteredData);
-    if (filteredData.length > 0) {
-      navigate(`/activities/${filteredData[0].id}`, { state: { activity: filteredData[0] } });
-    }
+    axios.get(`http://localhost:5001/api/activities/search?q=${searchTerm}`)
+      .then(response => {
+        setResults(response.data.data);
+        if (response.data.data.length > 0) {
+          navigate(`/activities/${response.data.data[0].id}`, { state: { activity: response.data.data[0] } });
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      });
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <input type="text" value={searchTerm} onChange={handleChange} />
       <button type="submit">Search</button>
-      {results.map(result => (
-       <div key={result.id} onClick={() => handleClick(result)}>
-       {result.title}
-     </div>
-    ))}
+      {results.length > 0 && (
+        <ul>
+          {results.map(result => (
+            <li key={result.id}>{result.title}</li>
+          ))}
+        </ul>
+      )}
     </form>
   );
 }
