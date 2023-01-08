deb721456a797ccfca9f97e20ede4de0200b6bd9
andreidorinm <andrei.dorin09@yahoo.com>
2023-01-08 23:44:35 +0200

    new changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import {BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.scss';
 import SearchActivityScreen from './screens/SearchActivityScreen';
 import FeedbackScreen from './screens/FeedbackScreen';
-import FeedbackSuccessScreen from './screens/FeedbackSuccessScreen';
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
     <Routes>
         <Route path="/activities" element={<SearchActivityScreen />} />
         <Route path="/activities/:id" element={<FeedbackScreen />} />
-        <Route path="/feedback" element={<FeedbackSuccessScreen />} />
     </Routes>
     </BrowserRouter>
   );
