3e8aea06ff87e9045871c0709eefe7616565958d
andreidorinm <andrei.dorin09@yahoo.com>
2023-01-14 02:38:28 +0200

    professor working

--- frontend/src/screens/ProfessorScreen.jsx
+++ frontend/src/screens/ProfessorScreen.jsx
@@ -1,11 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+import {
+  BsFillTrashFill,
+  BsFillPencilFill,
+  BsFillArchiveFill,
+} from 'react-icons/bs';
+import Modal from 'react-bootstrap/Modal';
+import './pages.scss';
 
 function ProfessorScreen() {
   const location = useLocation();
+  const navigate = useNavigate();
   const { professorId } = location.state;
   const [professor, setProfessor] = useState({});
+  const [activities, setActivities] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [updatedActivity, setUpdatedActivity] = useState({});
 
   useEffect(() => {
     if (professorId) {
@@ -18,13 +31,113 @@ function ProfessorScreen() {
           console.error(error);
         });
     }
+    axios
+      .get('http://localhost:5001/api/activities')
+      .then((response) => {
+        setActivities(response.data.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }, [professorId]);
+
+  const handleDelete = (activityId) => {
+    axios
+      .delete(`http://localhost:5001/api/activities/${activityId}`)
+      .then((response) => {
+        setActivities(
+          activities.filter((activity) => activity.id !== activityId)
+        );
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const handleUpdate = (activityId) => {
+    axios
+      .put(`http://localhost:5001/api/activities/${activityId}`, {
+        // data to be updated
+      })
+      .then((response) => {
+        setUpdatedActivity(response.data.data);
+        setShowModal(true);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const handleModalClose = () => {
+    setShowModal(false);
+  };
+
+  const handleCreate = () => {
+    navigate('/create-activity');
+  };
 
   return (
     <>
       <h1>Welcome, {professor.name}</h1>
       <div className="container-activities">
-        <div className="list-activities"></div>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {activities.map((activity) => (
+              <tr key={activity.id}>
+                <div className="container-activity-title">
+                  <td>{activity.title}</td>
+                </div>
+                <td>
+                  <Modal show={showModal} onHide={handleModalClose}>
+                    <Modal.Header closeButton>
+                      <Modal.Title>Update Activity</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                      {/* form fields for updating activity */}
+                    </Modal.Body>
+                    <Modal.Footer>
+                      <Button variant="secondary" onClick={handleModalClose}>
+                        Close
+                      </Button>
+                      {/* <Button variant="primary" onClick={handleUpdateSubmit}>
+                        Save Changes
+                      </Button> */}
+                    </Modal.Footer>
+                  </Modal>
+                  <div className="container-buttons">
+                    <Button
+                      className="button-create"
+                      variant="success"
+                      onClick={handleCreate}
+                    >
+                      <BsFillArchiveFill />
+                    </Button>
+                    <Button
+                      className="button-update"
+                      variant="warning"
+                      onClick={() => handleUpdate(activity.id)}
+                    >
+                      <BsFillPencilFill />
+                    </Button>
+                    <Button
+                      className="button-delete"
+                      variant="danger"
+                      onClick={() => handleDelete(activity.id)}
+                    >
+                      <BsFillTrashFill />
+                    </Button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
       </div>
     </>
   );
--- frontend/src/screens/signuplogin/LoginProfessorScreen.jsx
+++ frontend/src/screens/signuplogin/LoginProfessorScreen.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './screens.scss';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
 
 
